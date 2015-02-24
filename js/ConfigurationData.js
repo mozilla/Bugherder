439f36b22ad01a818ad61041299c95d99bb59df1
Ed Morley <emorley@mozilla.com>
2015-02-24 09:30:44 +0000

    Bug 1131862 - mcMerge: Return early if 'products' missing from bzapi config
    Check for the 'product' property up-front, rather than having conditionals on it
    throughout. Also create a variable for products, to clean up the references.

--- js/ConfigurationData.js
+++ js/ConfigurationData.js
@@ -41,23 +41,26 @@ var ConfigurationData = {
 
 
   parseData: function CD_parseData(data, loadCallback) {
+    if (!('product' in data)) {
+        loadCallback();
+        return;
+    }
+    var products = data.product;
     // Parse Milestones 
     var productMilestones = {}
-    if ('product' in data) {
-      for (var product in data['product']) {
-        var values = data.product[product].target_milestone;
-        productMilestones[product] = {}
-        productMilestones[product].values = values.map(UI.htmlEncode);
-        var dashIndex = values.indexOf('---');
-        if (dashIndex != -1) {
-          if (dashIndex + 1 < values.length && this.useNext.indexOf(product) != -1)
-            productMilestones[product].defaultIndex = dashIndex + 1;
-          else
-            productMilestones[product].defaultIndex = dashIndex;
-        } else
-          productMilestones[product].defaultIndex = 0;
+    for (var product in products) {
+      var values = products[product].target_milestone;
+      productMilestones[product] = {}
+      productMilestones[product].values = values.map(UI.htmlEncode);
+      var dashIndex = values.indexOf('---');
+      if (dashIndex != -1) {
+        if (dashIndex + 1 < values.length && this.useNext.indexOf(product) != -1)
+          productMilestones[product].defaultIndex = dashIndex + 1;
+        else
+          productMilestones[product].defaultIndex = dashIndex;
+      } else
+        productMilestones[product].defaultIndex = 0;
       }
-    }
     this.milestones = productMilestones;
 
     // Find the flag number for in-testsuite 
@@ -71,11 +74,11 @@ var ConfigurationData = {
     }
 
     // Find which products/components can have intestsuite set
-    if ('product' in data && this.testsuiteFlagID != -1) {
-      for (var product in data.product) {
+    if (this.testsuiteFlagID != -1) {
+      for (var product in products) {
         this.hasTestsuiteFlag[product] = {};
-        for (var component in data.product[product].component) {
-          var hasTestsuite = data.product[product].component[component].flag_type.indexOf(this.testsuiteFlagID) != -1;
+        for (var component in products[product].component) {
+          var hasTestsuite = products[product].component[component].flag_type.indexOf(this.testsuiteFlagID) != -1;
           this.hasTestsuiteFlag[product][component] = hasTestsuite;
         }
       }
