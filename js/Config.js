de4bb2994de7e10240afcc30f3bc3a2afb73d084
Wes Kocher <kwierso@gmail.com>
2015-06-04 13:43:31 -0700

    Add more support for b2g release flags
    And make them actually supported in Bugherder

--- js/Config.js
+++ js/Config.js
@@ -112,6 +112,18 @@ var Config = {
       trackedTree: true,
       synonyms: ["b2g34", "mozilla-b2g34", "mozilla-b2g34_v2_1", "v2.1"]
     },
+    "mozilla-b2g34_v2_1s": {
+      repo: "releases/mozilla-b2g34_v2_1s",
+      unconditionalFlag: true,
+      trackedTree: true,
+      synonyms: ["mozilla-b2g34_v2_1s", "v2.1s"]
+    },
+    "mozilla-b2g32_v2_0m": {
+      repo: "releases/mozilla-b2g32_v2_0m",
+      unconditionalFlag: true,
+      trackedTree: true,
+      synonyms: ["mozilla-b2g32_v2_0m", "v2.0m"]
+    },
     "mozilla-b2g32_v2_0": {
       repo: "releases/mozilla-b2g32_v2_0",
       unconditionalFlag: true,
