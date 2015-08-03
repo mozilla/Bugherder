29f5997534474ae932214086a1d0b668fbd2c2d7
KWierso <kwierso@gmail.com>
2015-08-03 12:00:50 -0700

    Bug 1160698 - Automatically select Firefox OS target milestones based on the current date

--- js/ConfigurationData.js
+++ js/ConfigurationData.js
@@ -25,6 +25,33 @@ var ConfigurationData = {
             'Thunderbird',
             'Toolkit'],
 
+  dateMilestones: [["FxOS-S1 (26Jun)", new Date(2015, 5, 26)],
+                   ["FxOS-S2 (10Jul)", new Date(2015, 6, 10)],
+                   ["FxOS-S3 (24Jul)", new Date(2015, 6, 24)],
+                   ["FxOS-S4 (07Aug)", new Date(2015, 7, 7)],
+                   ["FxOS-S5 (21Aug)", new Date(2015, 7, 21)],
+                   ["FxOS-S6 (04Sep)", new Date(2015, 8, 4)],
+                   ["FxOS-S7 (18Sep)", new Date(2015, 8, 18)],
+                   ["FxOS-S8 (02Oct)", new Date(2015, 9, 2)],
+                   ["FxOS-S9 (16Oct)", new Date(2015, 9, 16)],
+                   ["FxOS-S10 (30Oct)", new Date(2015, 9, 30)],
+                   ["FxOS-S11 (13Nov)", new Date(2015, 10, 13)],
+                   ["FxOS-S12 (27Nov)", new Date(2015, 10, 27)],
+                   ["FxOS-S13 (11Dec)", new Date(2015, 11, 11)],
+                   ["FxOS-S14 (25Dec)", new Date(2015, 11, 25)],
+                   ["FxOS-S15 (08Jan)", new Date(2016, 0, 8)],
+                   ["FxOS-S16 (22Jan)", new Date(2016, 0, 22)],
+                   ["FxOS-S17 (05Feb)", new Date(2016, 1, 5)],
+                   ["FxOS-S18 (19Feb)", new Date(2016, 1, 19)],
+                   ["FxOS-S19 (04Mar)", new Date(2016, 2, 4)],
+                   ["FxOS-S20 (18Mar)", new Date(2016, 2, 18)],
+                   ["FxOS-S21 (01Apr)", new Date(2016, 3, 1)],
+                   ["FxOS-S22 (15Apr)", new Date(2016, 3, 15)],
+                   ["FxOS-S23 (29Apr)", new Date(2016, 3, 29)],
+                   ["FxOS-S24 (13May)", new Date(2016, 4, 13)],
+                   ["FxOS-S25 (27May)", new Date(2016, 4, 27)],
+                   ["FxOS-S26 (10Jun)", new Date(2016, 5, 10)]],
+
 
   init: function CD_init(loadCallback, errorCallback) {
     var self = this;
@@ -69,6 +96,9 @@ var ConfigurationData = {
       if (dashIndex != -1) {
         if (dashIndex + 1 < active_milestones.length && this.useNext.indexOf(product) != -1) {
           productMilestones[product].defaultIndex = dashIndex + 1;
+        } else if (product == "Firefox OS") {
+          // B2G target milestones are date-based, handle these specially.
+          productMilestones[product].defaultIndex = productMilestones[product].values.indexOf(this.getDateMilestone());
         } else {
           productMilestones[product].defaultIndex = dashIndex;
         }
@@ -87,5 +117,18 @@ var ConfigurationData = {
     }
     this.milestones = productMilestones;
     loadCallback();
+  },
+
+  getDateMilestone: function CD_getDateMilestone(date) {
+    if (!date) {
+      date = Date.now();
+    }
+    var milestone;
+    for (var i = 1; i < this.dateMilestones.length; i++) {
+      if (date > this.dateMilestones[i-1][1] && date <= this.dateMilestones[i][1]) {
+        milestone = this.dateMilestones[i][0];
+      }
+    }
+    return milestone;
   }
 };
